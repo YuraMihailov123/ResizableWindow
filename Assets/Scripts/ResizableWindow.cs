c9ff328d66a8953615ff4563bc83292a0d4ab83a
YuraMihailov <recon.97@mail.ru>
2019-11-18 15:41:13 +0300

    task is ready
    ready

---
+++ Assets/Scripts/ResizableWindow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizableWindow : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.localPosition = text.gameObject.transform.localPosition;
+        UpdateWindowSize();
+    }
+
+    void UpdateWindowSize()
+    {
+        gameObject.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, text.preferredHeight);
+    }
+
+    public void IncreaseTextSize()
+    {
+        text.fontSize++;
+        UpdateWindowSize();
+    }
+
+}
